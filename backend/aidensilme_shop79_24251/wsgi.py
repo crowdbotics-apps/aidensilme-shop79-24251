41ec6dfb21736d66fa143142c65d44d44004055a
Crowdbotics <team@crowdbotics.com>
2021-02-03 17:47:38 +0000

    Initial commit.

---
+++ backend/aidensilme_shop79_24251/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aidensilme_shop79_24251 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aidensilme_shop79_24251.settings')
+
+application = get_wsgi_application()
